ef2f775f0806c70fb670d6c07ddc2a825d3d5b43
ajaysathvik <ajaysathvikk@gmail.com>
2023-08-20 11:37:00 +0530

    perfecting io and op

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
 fn main() {
-    println!("Hello, world!");
-    println!("I'm a AJAY");
+    // println!("Hello, world!");
+    // println!("I'm a AJAY");
 
-    let my_num = 50;
-    println!("My number is {}", my_num);
+    // let my_num = 50;
+    // println!("My number is {}", my_num);
 
-    let x = 5;
-    let y = 10;
-    println!("x = {} and y = {} and x+y ={}" , x, y,x+y);
+    // let x = 5;
+    // let y = 10;
+    // println!("x = {} and y = {} and x+y ={}" , x, y,x+y);
 
-    let z = x+y;
-    println!("z = {}", z);
+    // let z = x+y;
+    // println!("z = {}", z);
 
-    let a = 5;
-    let b = 10;
-    let z = a+b;
-    println!("a = {} and b = {} and a+b = {}", a,b,z);
+    // let mut a = 5;
+    // let b = 10;
+    // let z = a+b;
+    // println!("a = {} and b = {} and a+b = {}", a,b,z);
 
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Failed to read line");
-    println!("You entered {}", input);
+    // let mut input = String::new(); // mutable variable 
+    // std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    // print!("You entered {}", input);
+
+    // println!("{}",a*b);
+    // println!("{}",a/b);
+    // println!("{}",a%b);
+    // println!("{}",a-b);
+
+    // a+=1;
+    // println!("{}",a);
+
+    // let mut name = String::new();
+    // std::io::stdin().read_line(&mut name).expect("Failed to read line");
+    // let greetings = String::from("Hello");
+    // name.replace_range(0..1, ""); // replace the first character of the string with empty string
+    // name = name.trim().to_string(); // trim() is used to remove the white spaces
+    // println!("{} {}",greetings,name);
+    // println!("{}",greetings.len());
+    // println!("{}",name.len());
+
+    
 }
