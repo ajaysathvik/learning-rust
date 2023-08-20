c13ce207ae25f2fa8d8827330dd81f5bb134661b
ajaysathvik <ajaysathvikk@gmail.com>
2023-08-20 10:53:11 +0530

    input and printing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
+    println!("I'm a AJAY");
+
+    let my_num = 50;
+    println!("My number is {}", my_num);
+
+    let x = 5;
+    let y = 10;
+    println!("x = {} and y = {} and x+y ={}" , x, y,x+y);
+
+    let z = x+y;
+    println!("z = {}", z);
+
+    let a = 5;
+    let b = 10;
+    let z = a+b;
+    println!("a = {} and b = {} and a+b = {}", a,b,z);
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    println!("You entered {}", input);
 }
