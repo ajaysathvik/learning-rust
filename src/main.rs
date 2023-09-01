3797fd061abfeec5637590d34da08c3743dbc213
ajaysathvik <ajaysathvikk@gmail.com>
2023-09-01 15:03:05 +0530

    struct deep

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 // use std::f64;
 // use rand::Rng;
 
+use std::process::Stdio;
+
 fn main() {
     // println!("Hello, world!");
     // println!("I'm a AJAY");
@@ -160,8 +162,8 @@ fn main() {
 
     // let str1 = String::from("Hello");
     // let str2 = str1;
-    // println!("{}\n{}", str1, str2); // no error  
-    
+    // println!("{}\n{}", str1, str2); // no error
+
     // let s1 = String::from("Ajay");
     // let len = calculate_length(&s1);
     // println!("The length of '{}' is {}.", s1, len);
@@ -174,7 +176,7 @@ fn main() {
     // let hello2 = &s[..5];
     // println!("{}\n{}\n{}", hello, world, all);
     // println!("{}\n{}", world2, hello2);
-    
+
     // #[derive(Debug)]
     // struct User {
     //     username: String,
@@ -200,7 +202,50 @@ fn main() {
     // println!("{} \n{} \n{} \n{}", user1.username, user1.email, user1.sign_in_count, user1.active);
 
     /* DAY8 */
-    
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+    // impl Rectangle {
+    //     fn area(&self) -> u32 {
+    //         self.width * self.height
+    //     }
+    //     fn can_hold(&self, other: &Rectangle) -> bool {
+    //         self.width > other.width && self.height > other.height
+    //     }
+    //     fn square(size: u32) -> Rectangle {
+    //         Rectangle {
+    //             width: size,
+    //             height: size,
+    //         }
+    //     }
+    // } // impl is used to define methods for a structure
+    // let rect1 = Rectangle {
+    //     width: 30,
+    //     height: 50,
+    // };
+    // let rect2 = Rectangle {
+    //     width: 10,
+    //     height: 40,
+    // };
+    // let rect3 = Rectangle {
+    //     width: 60,
+    //     height: 45,
+    // };
+    // println!("The area of the rectangle is {} square pixels.", rect1.area());
+    // println!("rect1 can hold rect2: {}", rect1.can_hold(&rect2));
+    // println!("rect1 can hold rect3: {}", rect1.can_hold(&rect3));
+    // let sq = Rectangle::square(3);
+    // println!("The area of the square is {} square pixels.", sq.area());
+
+    // let rect1 = Rectangle {
+    //     width: 30,
+    //     height: 50,
+    // };
+    // fn area(rectangle: Rectangle) -> u32 {
+    //     rectangle.width * rectangle.height
+    // }
+    // print!("{}", area(rect1));
 }
 
 // fn calculate_length(s: &String) -> usize {
